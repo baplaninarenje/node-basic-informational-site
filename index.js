4b4deac29c61933797d50d6374d479cca2de844c
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2025-03-28 14:27:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require('node:http');
+const fs = require('node:fs/promises');
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log(req.url);
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  readFileAsync(getFileNameFromReqUrl(req.url)).then((f) => res.end(f));
+});
+
+server.listen(8080);
+
+async function readFileAsync(filename) {
+  try {
+    return await fs.readFile(filename, { encoding: 'utf8' });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const getFileNameFromReqUrl = (reqUrl) => {
+  switch (reqUrl) {
+    case '/':
+      return 'index.html';
+    case '/about':
+      return 'about.html';
+    case '/contact-me':
+      return 'contact-me.html';
+
+    default:
+      return '404.html';
+  }
+};
